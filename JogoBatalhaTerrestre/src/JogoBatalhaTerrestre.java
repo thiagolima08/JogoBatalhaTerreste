af63caf9b8879bdd460fdec7aa02a14a300e80db
Thiago Lima <44410746+thiagolima08@users.noreply.github.com>
2019-10-29 20:19:05 -0300

    Update JogoBatalhaTerrestre.java

--- JogoBatalhaTerrestre/src/JogoBatalhaTerrestre.java
+++ JogoBatalhaTerrestre/src/JogoBatalhaTerrestre.java
@@ -17,165 +17,77 @@ public class JogoBatalhaTerrestre {
 		array_linha = new int[20];
 		array_coluna = new int[20];
 		Random rand = new Random();
-		int n1 = rand.nextInt(10);
-	    int n2 = rand.nextInt(10);
-	   
+			   
 	    int cont = 0;
 	    while (cont<=4) {
+	    	int n1 = rand.nextInt(10);
+		    int n2 = rand.nextInt(10);
 	    	if(matriz[n1][n2]==0) {
 	    		matriz[n1][n2]=1;
-	    	}else {
-	    		n1 = rand.nextInt(10);
-				n2 = rand.nextInt(10);
-				if(matriz[n1][n2]==0) {
-		    		matriz[n1][n2]=1;
-				}else {
-		    		n1 = rand.nextInt(10);
-					n2 = rand.nextInt(10);
-					matriz[n1][n2]=1;
-					}
+	    		cont++;
 	    	}
-	    	cont++;
+	    	else {
+	    		cont--;
+				}
 	    }
 	}
 
 	public String atirar(int linha, int coluna)  throws Exception {
-		//validar linha e coluna fora da faixa
 		
+		// verifica se a linha e a coluna são repetidas
+		for(int i=0; i<getTiros(); i++){
+				if(array_linha[i]==linha && array_coluna[i]==coluna){
+					throw new Exception("Já atirou essa posição, escolha outra.");							
+						}
+				}
+		
+		//validar linha e coluna fora da faixa
 		if(linha<0 || linha>9 || coluna<0 || coluna>9 ) {
 			throw new Exception("o número da linha ou coluna esta fora da faixa permitida entre 0 e 9"); 
 		}
 
-		// Sendo linha e coluna válidas pode atirar
+		// Sendo linha e coluna válidas é possivel atirar
 		
-		else{
-			FileWriter arq = new FileWriter("result.txt", true);
-			BufferedWriter conexao = new BufferedWriter(arq);
+		FileWriter arq = new FileWriter("result.txt", true);
+		BufferedWriter conexao = new BufferedWriter(arq);
+		
+		// Armazenando os locais que já atiraram
+
+		array_linha[getTiros()]=linha;
+		array_coluna[getTiros()]=coluna;
 			
-			if (matriz[linha][coluna]==1) {
-				System.out.println("Alvo\n");
+		if (matriz[linha][coluna]==1) {
 				setAcertos(1);
 				setTiros(1);
 				
 				// Salvando em arquivo texto
-				String linha0;
-				linha0 = "Alvo";
-				conexao.write(linha+" "+coluna+" "+linha0);
-				conexao.newLine();
-				conexao.close();
-				
-				
-				// verifica se a linha e a coluna já foi acertada  
-				if(!(array_linha==null)){
-					for(int i=1; i<getTiros(); i++){
-						if(array_linha[i]==linha && array_coluna[i]==coluna){
-							setAcertos(-1);
-							throw new Exception("Já acertou essa posição, escolha outra.");							
-							}
-					}
-				}
-
-				for(int i=getTiros(); i<=getTiros(); i++){
-					array_linha[i]=linha;
-					array_coluna[i]=coluna;
-					}
+					conexao.write(linha+" "+coluna+" "+"Alvo");
+					conexao.newLine();
+					conexao.close();
+					System.out.println("Alvo\n");
+				return "Alvo\n";
 			}
-
-			else{
-				setTiros(1);
-				int soma=0;
-				
-				//verifica se existem alvos vizinhos ao tiro
-				
-				if (linha==9&&coluna==9) {
-					for(int i=linha-1;i<linha+1;i++) {
-						for(int j=coluna-1;j<coluna+1;j++) {
-							if (matriz[i][j]==1) {
-								soma++;							
-								}
-							}
-						}
-				}else if(linha==0&&coluna==0){
-					for(int i=linha;i<linha+2;i++) {
-						for(int j=coluna;j<coluna+2;j++) {
-							if (matriz[i][j]==1) {
-								soma++;							
-							}
-						}
-					}
-				}
-				else {
-					if (linha==9||coluna==0) {
-						for(int i=linha-1;i<linha+1;i++) {
-							for(int j=coluna;j<coluna+2;j++) {
-								if (matriz[i][j]==1) {
-									soma++;							
-									}
-								}
-							}
-					}else if(linha==0||coluna==9){
-						for(int i=linha;i<linha+2;i++) {
-							for(int j=coluna-1;j<coluna+1;j++) {
-								if (matriz[i][j]==1) {
-									soma++;							
-								}
-							}
-						}
-					}
-					else {
-					for(int i=linha-1;i<linha+2;i++) {
-						for(int j=coluna-1;j<coluna+2;j++) {
-							if (matriz[i][j]==1) {
-								soma++;							
-								}
-							}
-						}
-					}
-				}
-				
-				//quantidade de alvos próximos ao tiro
-				
-				if(soma>=1) {
-					System.out.println("Próximo de acertar "+soma+" dos alvos\n");
-					
-					// Salvando em arquivo texto
-
-					String linha1;
-					linha1 = "Próximo de acertar o alvo";
-					conexao.write(linha+" "+coluna+" "+linha1);
+		else{
+			setTiros(1);
+			
+			if (temAlvoPerto(linha,coluna)==true) {
+				// Salvando em arquivo texto
+					conexao.write(linha+" "+coluna+" "+"Próximo");
 					conexao.newLine();
 					conexao.close();
-					}
-				else {
+					System.out.println("Próximo\n");
+				return "Próximo\n"; 
+			}else {
+				//Salvando em arquivo texto
+					conexao.write(linha+" "+coluna+" "+"Distante");
+					conexao.newLine();
+					conexao.close();
 					System.out.println("Distante\n");
-					
-					//Salvando em arquivo texto
-					
-					String linha2;
-					linha2 = "Distante";
-					conexao.write(linha+" "+coluna+" "+linha2);
-					conexao.newLine();
-					conexao.close();
-					}
-				
-				// Armazenando os locais que já atiraram
-				
-				if(!(array_linha==null)){
-					for(int i=1; i<getTiros(); i++){
-						if(array_linha[i]==linha && array_coluna[i]==coluna){
-							throw new Exception("Já atirou nessa posição, escolha outra.");
-							}
-					}
-				}
-
-				for(int i=getTiros(); i<=getTiros(); i++){
-					array_linha[i]=linha;
-					array_coluna[i]=coluna;
-					}
-			}				
-		}
-		return null;
+				return "Distante\n";
+			}
+		}			
 	}
+
 				
 	public int getAcertos() {
 		return acertos;
@@ -200,6 +112,62 @@ public class JogoBatalhaTerrestre {
 		}
 		return true;
 		
+	}
+	
+	private boolean temAlvoPerto(int linha, int coluna) {
+		//verifica se existem alvos vizinhos ao tiro
+		int soma =0;
+		if (linha==9&&coluna==9) {
+			for(int i=linha-1;i<linha+1;i++) {
+				for(int j=coluna-1;j<coluna+1;j++) {
+					if (matriz[i][j]==1) {
+						soma++;							
+						}
+					}
+				}
+		}else if(linha==0&&coluna==0){
+			for(int i=linha;i<linha+2;i++) {
+				for(int j=coluna;j<coluna+2;j++) {
+					if (matriz[i][j]==1) {
+						soma++;							
+					}
+				}
+			}
+		}
+		else {
+			if (linha==9||coluna==0) {
+				for(int i=linha-1;i<linha+1;i++) {
+					for(int j=coluna;j<coluna+2;j++) {
+						if (matriz[i][j]==1) {
+							soma++;							
+							}
+						}
+					}
+			}else if(linha==0||coluna==9){
+				for(int i=linha;i<linha+2;i++) {
+					for(int j=coluna-1;j<coluna+1;j++) {
+						if (matriz[i][j]==1) {
+							soma++;							
+						}
+					}
+				}
+			}
+			else {
+			for(int i=linha-1;i<linha+2;i++) {
+				for(int j=coluna-1;j<coluna+2;j++) {
+					if (matriz[i][j]==1) {
+						soma++;							
+						}
+					}
+				}
+			}
+		}
+		if(soma>=1) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 	
 	public String toString() {
